e4b3d6ab48936f0b3cdb21124550489138c2232c
Naomi Rodriguez <naomirodriguez@naomis-mbp.t-com.me>
2021-09-29 12:27:24 +0200

    Swapped out URI inside of index.js file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("'mongodb://localhost:27017/myFlixDB'", {
+mongoose.connect(process.env.CONNECTION_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
